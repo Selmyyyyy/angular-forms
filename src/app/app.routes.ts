2f394f0f2cc529f3114488e22ce697a11d1fbf04
Selmyyyyy <salmasobhi2200@gmail.com>
2026-02-11 09:15:15 +0100

    riguardolo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { FormTwo } from './components/form-two/form-two';
+import { FormOne } from './components/form-one/form-one';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"", redirectTo:"/form1", pathMatch:"full"},
+    {path:"form1", component: FormOne},
+    {path:"form2", component: FormTwo},
+    {path:"**", redirectTo:"/form1", pathMatch:"full"}
+
+];
---
+++ src/app/components/form-one/form-one.ts
@@ -0,0 +1,62 @@
+import { Component, inject } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-one',
+  imports: [ReactiveFormsModule],
+  templateUrl: './form-one.html',
+  styleUrl: './form-one.scss',
+})
+export class FormOne {
+
+
+
+  // profileForm = new FormGroup({
+  //   name: new FormControl("pippo"),
+  //   surname: new FormControl("de pippis"),
+  //   adress: new FormGroup({
+  //     city: new FormControl('topolinia'),
+  //     street: new FormControl('corso perrone'),
+  //     number: new FormControl(12)
+  //   })
+  // });
+
+  fb = inject(FormBuilder);
+  
+  profileForm = this.fb.group({
+    name: ['pippo,', Validators.required],
+      surname:['de pippis', Validators.maxLength(4)],
+      adress: this.fb.group({
+        city:['topolinia'],
+        street:['corso perrone'],
+        number:[12, [Validators.min(1),Validators.required]],
+      }),
+    aliases: this.fb.array([
+      this.fb.control(''),
+     
+    ]),
+    
+  })
+
+
+  submit() {
+console.log(this.profileForm.value);
+
+  }
+
+  getAliases(){
+    return this.profileForm.get('aliases') as FormArray;
+  }
+
+  addAlias() {
+  this.getAliases().controls.push(this.fb.control(''))
+  }
+
+  removeAlias(index: number) {
+    console.log(index);
+    
+this.getAliases().removeAt(index);
+}
+
+
+}
